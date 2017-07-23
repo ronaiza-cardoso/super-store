250e1f3554c836eca5b4b21b35be1facc26a36c3
Ronaiza Cardoso <ronaizacard@gmail.com>
2017-07-23 11:54:53 -0300

    Adding components of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { SearchbarComponent } from './componentes/searchbar/searchbar.component';
+import { CartbarComponent } from './componentes/cartbar/cartbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { ProductListComponent } from './componentes/product-list/product-list.component';
+import { ProductCardComponent } from './componentes/product-card/product-card.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { Erro404PageComponent } from './pages/erro-404-page/erro-404-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LogoComponent,
+    SearchbarComponent,
+    CartbarComponent,
+    BannerComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductPageComponent,
+    ProductsPageComponent,
+    Erro404PageComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/erro-404-page/erro-404-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-erro-404-page',
+  templateUrl: './erro-404-page.component.html',
+  styleUrls: ['./erro-404-page.component.sass']
+})
+export class Erro404PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
